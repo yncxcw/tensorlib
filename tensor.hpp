db7c4390bbfc60c2cce347ebb3c580e88f32ad54
Wei Chen <weich@nvidia.com>
2022-10-26 22:11:30 -0600

    add copy constructor

--- tensor.hpp
+++ tensor.hpp
@@ -1,94 +1,99 @@
 #pragma once
 
-#include<cassert>
-#include<cstdio>
-#include<cstring>
-#include<type_traits>
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <type_traits>
 
-template<typename T, size_t... Ns>
-class TensorDelegate;
+template <typename T> T sum_array(const T *t, size_t length) {
+  T sum = 0;
+  for (size_t i = 0; i < length; i++) {
+    sum += t[i];
+  }
+  return sum;
+}
 
-template<typename T, size_t N>
-class TensorDelegate<T, N> {
-    public:
-        TensorDelegate(T* data): data(data) {}
-        T& operator[](const size_t idx) {
-            assert(idx < N);
-            return data[idx];
-        }
-    private:
-        T *data;
+template <typename T, size_t... Ns> class TensorDelegate;
+
+template <typename T, size_t N> class TensorDelegate<T, N> {
+public:
+  TensorDelegate(T *data) : data(data) {}
+  T &operator[](const size_t idx) {
+    assert(idx < N);
+    return data[idx];
+  }
+
+private:
+  T *data;
 };
 
-template<typename T, size_t N, size_t... Ns>
+template <typename T, size_t N, size_t... Ns>
 class TensorDelegate<T, N, Ns...> {
-    public:
-        TensorDelegate(T* data): data(data) {}
-        TensorDelegate<T, Ns...> operator[](const size_t idx) {
-            assert(idx < N);
-            return TensorDelegate<T, Ns...>(data + idx * stride);
-        }
-    private:
-        static constexpr size_t stride = (... * Ns);
-        T* data;
+public:
+  TensorDelegate(T *data) : data(data) {}
+  TensorDelegate<T, Ns...> operator[](const size_t idx) {
+    assert(idx < N);
+    return TensorDelegate<T, Ns...>(data + idx * stride);
+  }
+
+private:
+  static constexpr size_t stride = (... * Ns);
+  T *data;
 };
 
-template<class T, size_t... N>
-class Tensor;
+template <class T, size_t... N> class Tensor;
 
-template<class T, size_t N, size_t...Ns>
-class Tensor<T, N, Ns...> {
-    public:
-        Tensor(){
-            data = new T[size];     
-        }
-        ~Tensor() {
-            delete data;
-        }
-        Tensor(const Tensor& tensor) {
-            data = new T[size];
-            std::memcpy(data, tensor.data, size*sizeof(T));
-        }
-        static size_t get_size() {
-            return size;
-        }
-        T* get_data() const {
-            return data;
-        }
-        TensorDelegate<T, Ns...> operator[] (const size_t idx) {
-            assert(idx < N);
-            return TensorDelegate<T, Ns...>(data + idx * stride);
-        } 
-    private:
-        T* data;
-        constexpr static size_t size = N * (... * Ns);
-        constexpr static size_t stride = (... * Ns);
+template <class T, size_t N, size_t... Ns> class Tensor<T, N, Ns...> {
+public:
+  Tensor() {}
+  Tensor(const T v) {
+    for (size_t i = 0; i < size; i++) {
+      data[i] = std::move(v);
+    }
+  }
+  ~Tensor() {}
+  Tensor(const Tensor<T, N, Ns...> &tensor) {
+    static_assert(tensor.size == size);
+    std::memcpy(data, tensor.data, size * sizeof(T));
+  }
+  constexpr size_t get_size() const { return size; }
+  T *get_data() const { return data; }
+  TensorDelegate<T, Ns...> operator[](const size_t idx) {
+    assert(idx < N);
+    return TensorDelegate<T, Ns...>(data + idx * stride);
+  }
+  T sum() { return sum_array<T>(data, size); }
+
+private:
+  constexpr static size_t size = N * (... * Ns);
+  constexpr static size_t stride = (... * Ns);
+  T data[size];
 };
 
-template<class T, size_t N>
-class Tensor<T, N> {
-    public:
-        Tensor(){
-            data = new T[size];     
-        }
-        ~Tensor() {
-            delete data;
-        }
-        Tensor(const Tensor& tensor) {
-            data = new T[size];
-            std::memcpy(data, tensor.data, size*sizeof(T));
-        }
-        static size_t get_size() {
-            return size;
-        }
-        T* get_data() const {
-            return data;
-        }
-        T& operator[] (const size_t idx) {
-            assert(idx < N);
-            return data[idx];
-        } 
-    private:
-        T* data;
-        constexpr static size_t size = N;
+template <class T, size_t N> class Tensor<T, N> {
+public:
+  Tensor() {}
+  Tensor(const T v) {
+    for (size_t i = 0; i < size; i++) {
+      data[i] = std::move(v);
+    }
+  }
+  ~Tensor() {}
+  Tensor(const Tensor<T, N> &tensor) {
+    static_assert(tensor.size == size);
+    std::memcpy(data, tensor.data, size * sizeof(T));
+  }
+  constexpr size_t get_size() const { return size; }
+  T *get_data() const { return data; }
+  T &operator[](const size_t idx) {
+    assert(idx < N);
+    return data[idx];
+  }
+
+  T sum() { return sum_array<T>(data, size); }
+
+private:
+  constexpr static size_t size = N;
+  T data[size];
 };
--- tests/test_1d_tensor.hpp
+++ tests/test_1d_tensor.hpp
@@ -1,20 +1,23 @@
 #pragma once
 
-#include <gtest/gtest.h>
 #include "../tensor.hpp"
+#include <gtest/gtest.h>
 
 TEST(TEST1D, Test_size) {
-    Tensor<double, 10> tensor;
-    int size = tensor.get_size();
-    EXPECT_EQ(size, 10);
-    size = Tensor<double, 10>::get_size();
-    EXPECT_EQ(size, 10);
+  Tensor<double, 10> tensor;
+  int size = tensor.get_size();
+  EXPECT_EQ(size, 10);
 }
 
 TEST(TEST1D, Test_value) {
-    Tensor<double, 10> tensor;
-    tensor[0] = 10.0;
-    tensor[1] = tensor[0] + 20.0; 
-    EXPECT_EQ(tensor[0], 10.0);
-    EXPECT_EQ(tensor[1], 30.0);
+  Tensor<double, 10> tensor;
+  tensor[0] = 10.0;
+  tensor[1] = tensor[0] + 20.0;
+  EXPECT_EQ(tensor[0], 10.0);
+  EXPECT_EQ(tensor[1], 30.0);
+}
+
+TEST(TEST1D, Test_sum) {
+  Tensor<double, 10> tensor(0.1);
+  EXPECT_DOUBLE_EQ(tensor.sum(), 1.0);
 }
